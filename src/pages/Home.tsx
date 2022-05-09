ff544e7732a90be511595974f103306b95aa1025
nirpa hang rai <nirpa@iugo.com.au>
2022-05-09 10:27:30 +0930

    IonTitle updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tenzies</IonTitle>
+          <IonTitle>Let's Play</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Tenzies</IonTitle>
           </IonToolbar>
         </IonHeader>
         <TenziesContainer />
